50f24180766973f5a0e5f9ef33252a8451f923df
root <root@localhost.localdomain>
2018-05-11 19:06:27 +0530

    songs playing simultaneously in p5 sketch

--- sketch.js
+++ sketch.js
@@ -1,33 +1,31 @@
-var song;
+var song,myCallback = function() {
+  //alert("Song Stopped");
+  /*var num = Math.floor(Math.random() * 105);
+  song = loadSound(num.toString()+'.webm');
+  song.onended(myCallback);*/
+  preload;
+  song.play();
+  song.onended(myCallback);
+       };
 var amp;
 var button;
 
 var volhistory = [];
 
-function toggleSong() {
-  if (song.isPlaying()) {
-    song.pause();
-  } else {
-    song.play();
-  }
-}
 
 function preload() {
-  var a = Math.floor(Math.random() * 105);
-  song = loadSound(a.toString()+'.webm');
-  console.log(a);
-  song.onended = function(){
-	var num = Math.floor(Math.random() * 105);
-	song = loadSound(num.toString()+'.webm');
-};
+  var songNum = Math.floor(Math.random() * 105);
+  console.log(songNum);
+  song = loadSound(songNum+'.webm');
+  //song.onended(myCallback);
 }
 
 function setup() {
   createCanvas(200, 200);
   angleMode(DEGREES);
-  button = createButton('toggle');
-  button.mousePressed(toggleSong);
+  
   song.play();
+  song.onended(myCallback);
   amp = new p5.Amplitude();
 }
 
@@ -51,5 +49,4 @@ function draw() {
   if (volhistory.length > 360) {
     volhistory.splice(0, 1);
   }
-  //ellipse(100, 100, 200, vol * 200);
 }
